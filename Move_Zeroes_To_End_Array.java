b556159f73f6b8650b6fd8f9b0155cc50e5faa72
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2024-10-19 23:01:34 +0530

    A meaningful commit message

--- Move_Zeroes_To_End_Array.java
+++ Move_Zeroes_To_End_Array.java
@@ -1,22 +1,20 @@
-public class Move_Zeroes_To_End_Array {
+public class MoveZeroesToEndArray {
 
     public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
         System.out.println();
     }
 
     public static void moveZeroes(int[] arr) {
-        int j = 0;
+        int j = 0; 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0 && arr[j] == 0) {
+            if (arr[i] != 0) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-            }
-            if (arr[j] != 0) {
-                j++;
+                j++; 
             }
         }
     }
@@ -24,12 +22,11 @@ public class Move_Zeroes_To_End_Array {
     public static void main(String[] args) {
         int[] arr = { 0, 1, 0, 12, 0, 0, 23, 24 };
 
-        System.err.println("Original Array");
+        System.out.println("Original Array");
         printArray(arr);
 
         System.out.println("Array after moving zeroes to end");
         moveZeroes(arr);
         printArray(arr);
     }
-
 }
